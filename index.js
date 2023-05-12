2a7e50b3c85c7c23a979e4a20bcdd360390e3477
joaocarlosweb <joaocarloswebcod@gmail.com>
2023-05-12 08:19:46 -0300

    atualizando
    atualização do projeto api tempo

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function dadosNaTela(dados){
+    document.querySelector(".cidade1").innerHTML="Tempo em: "+dados.name
+    document.querySelector(".temp").innerHTML= dados.main.temp+" ºC"
+    document.querySelector(".texto-previsao").innerHTML=dados.weather[0].description
+    document.querySelector(".humidade").innerHTML= "Humidade "+dados.main.humidity+"%"
+    document.querySelector(".inco-previsao").src=`https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    document.querySelector(".grau").innerHTML=dados.wind.deg+ " graus acima do mar"
+    
+
+    console.log(dados);
+}
+
+
+async function buscar(cidade){
+    const dados= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta=> resposta.json())
+     dadosNaTela(dados);
+}
+
+
+const key="bbf6bb38e43d1b6e8aea5ff4025e1663"
+function cliquei(){
+   const cidade= document.querySelector(".cidade").value;
+   
+    buscar(cidade);
+}
